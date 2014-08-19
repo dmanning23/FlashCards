03c5aeb7e83ed2fbc665dac2f99639272c279fb9
unknown <dmanning23@hotmail.com>
2014-08-18 20:07:14 -0500

    initial commit

---
+++ Source/QuestionScreen.cs
@@ -0,0 +1,193 @@
+using System.Collections.Generic;
+using ListExtensions;
+using System.Diagnostics;
+using MenuBuddy;
+using GameTimer;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlashCards
+{
+	/// <summary>
+	/// Used to communicate with the combat engine if the user gave a correct answer
+	/// </summary>
+	/// <param name="correctAnswer"></param>
+	public delegate void AnsweredCorrectly(bool correctAnswer);
+
+	/// <summary>
+	/// This is a menu screen that has a thing to translate, and then four possible options.
+	/// The user has to choose one of the options, but only one of the answers is correct. 
+	/// </summary>
+	public class QuestionScreen : MenuScreen
+	{
+		#region Member Variables
+
+		/// <summary>
+		/// this timer is used to automatically choose an answer
+		/// also reused to display the correct ansnwer and exit the screen
+		/// </summary>
+		private CountdownTimer _autoQuit = new CountdownTimer();
+
+		/// <summary>
+		/// whether or not the player got the question right
+		/// </summary>
+		public bool AnsweredCorrect { get; private set; }
+
+		/// <summary>
+		/// Flag set when the user chooses an answer, either right or wrong
+		/// </summary>
+		private bool AnswerChosen { get; set; }
+
+		/// <summary>
+		/// the correct answer
+		/// </summary>
+		private MenuEntry CorrectAnswer { get; set; }
+
+		Random _rand = new Random();
+
+		/// <summary>
+		/// method we will call when the user has answered
+		/// </summary>
+		private AnsweredCorrectly Answered { get; set; }
+
+		#endregion //Member Variables
+
+		#region Initialization
+
+		/// <summary>
+		///	hello, standard constructor!
+		/// </summary>
+		public QuestionScreen(
+			AnsweredCorrectly answered,
+			string question,
+			string correctAnswer,
+			List<string> wrongAnswers)
+			: base(question)
+		{
+			Debug.Assert(null != answered);
+			Debug.Assert(null != wrongAnswers);
+
+			//this screen should transition on really slow for effect
+			TransitionOnTime = TimeSpan.FromSeconds(0.5f);
+
+			//no need for game to transition off when this screen is up
+			IsPopup = true;
+
+			//set up the question
+			Answered = answered;
+			AnsweredCorrect = false;
+			AnswerChosen = false;
+
+			//create the correct menu entry
+			CorrectAnswer = new MenuEntry(correctAnswer);
+			CorrectAnswer.Selected += CorrectAnswerSelected;
+			MenuEntries.Add(CorrectAnswer);
+
+			//Add exactly three wrong answers
+			Debug.Assert(3 == wrongAnswers.Count);
+			for (int i = 0; i < 3; i++)
+			{
+				//create a menu entry for that answer
+				var wrongMenuEntry = new MenuEntry(wrongAnswers[i]);
+				wrongMenuEntry.Selected += WrongAnswerSelected;
+				MenuEntries.Add(wrongMenuEntry);
+			}
+
+			//shuffle the answers
+			MenuEntries.Shuffle(_rand);
+
+			//make the player stare at this screen for 2 seconds before they can quit
+			_autoQuit.Start(10.0f);
+		}
+
+		#endregion //Initialization
+
+		#region Handle Input
+
+		/// <summary>
+		/// Event handler for when the High Scores menu entry is selected.
+		/// </summary>
+		private void CorrectAnswerSelected(object sender, PlayerIndexEventArgs e)
+		{
+			AnswerSelected(true);
+		}
+
+		/// <summary>
+		/// Event handler for when the High Scores menu entry is selected.
+		/// </summary>
+		private void WrongAnswerSelected(object sender, PlayerIndexEventArgs e)
+		{
+			AnswerSelected(false);
+		}
+
+		#endregion //Handle Input
+
+		#region Methods
+
+		public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+		{
+			//update the timers
+			_autoQuit.Update(gameTime);
+
+			//check if we been here long enough
+			if (0.0f >= _autoQuit.RemainingTime())
+			{
+				//has the user picked an answer?
+				if (!AnswerChosen)
+				{
+					//the timer ran out but the user hadn't picked an answer.  That counts as "wrong"
+					AnswerSelected(false);
+				}
+				else
+				{
+					//holla at the combat engine
+					Answered(AnsweredCorrect);
+					ExitScreen();
+				}
+			}
+
+			base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+		}
+
+		/// <summary>
+		/// The user selected an answer.
+		/// Set the flags, menu entry colors, and start timer.
+		/// </summary>
+		private void AnswerSelected(bool correctAnswer)
+		{
+			//set flags
+			AnswerChosen = true;
+			AnsweredCorrect = correctAnswer;
+
+			//play the appropriate sound effect
+			if (AnsweredCorrect)
+			{
+				//TODO: play "KA-CHING" sound effect
+			}
+			else
+			{
+				//TODO: play "BOI-OING" sound effect
+			}
+
+			//set all menu entries to bright red
+			foreach (var entry in MenuEntries)
+			{
+				entry.NonSelectedColor = Color.Red;
+			}
+
+			//...but then change the correct answer to green.
+			CorrectAnswer.NonSelectedColor = new Color(0.0f, 0.7f, 0.0f);
+
+			//start the timer to exit this screen
+			_autoQuit.Start(0.5f);
+		}
+
+		protected override void OnCancel(PlayerIndex playerIndex)
+		{
+			//Do nothing if the user cancels a question screen
+		}
+
+		#endregion //Methods
+	}
+}
---
+++ Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FlashCards.Windows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("FlashCards.Windows")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("73f4cfee-a2b8-4f72-a60e-b5e69dd3e03c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
